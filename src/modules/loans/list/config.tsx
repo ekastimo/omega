dc326506ebb94c73368d6e9ee024fea650976f90
Timothy Emmanuel Kasasa <timothyk@laboremus.no>
2020-04-05 17:50:33 +0300

    Fix view data

--- src/modules/loans/list/config.tsx
+++ src/modules/loans/list/config.tsx
@@ -125,15 +125,18 @@ export const columns: XHeadCell[] = [
     }
 ];
 
+const removeColumns = (cols: XHeadCell[], filter: string[]) => {
+    return columns.filter(it => {
+        return filter.indexOf(it.name) < 0
+    }).map(({cellProps, ...rest}) => ({...rest}))
+}
 
-export const recentLoansHeadCells: XHeadCell[] = [...columns.filter(it => {
-    return it.name !== 'assigneeId'
-}).map(({cellProps, ...rest}) => ({...rest}))];
+export const recentLoansHeadCells: XHeadCell[] = [...removeColumns(columns,['assigneeId'])];
 
-export const companyLoansHeadCells: XHeadCell[] = [...columns];
-export const personLoansHeadCells: XHeadCell[] = [...columns.filter(it => {
-    return it.name !== 'applicantId'
-}).map(({cellProps, ...rest}) => ({...rest}))];
+
+
+export const companyLoansHeadCells: XHeadCell[] = [...removeColumns(columns,['assigneeId'])];
+export const personLoansHeadCells: XHeadCell[] = [...removeColumns(columns,['assigneeId','applicantId'])];
 
 export const contactLoanSumHeaderCells: XHeadCell[] = [
     {
@@ -154,7 +157,8 @@ export const contactLoanSumHeaderCells: XHeadCell[] = [
     {
         name: 'applicantId',
         label: 'Applicant',
-        render: (value, rec) => <ContactLink id={value} title={rec.applicant.name} name={getInitials(rec.applicant.name)}/>,
+        render: (value, rec) => <ContactLink id={value} title={rec.applicant.name}
+                                             name={getInitials(rec.applicant.name)}/>,
     },
     {
         name: 'amount', label: 'Amount', render: value => printMoney(value)
