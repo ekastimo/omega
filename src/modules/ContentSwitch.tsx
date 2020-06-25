314afccb9f6162e38c656c267c59884cef5e877d
Timothy Emmanuel Kasasa <timothyk@laboremus.no>
2020-06-25 11:28:17 +0300

    clean up data

--- src/modules/ContentSwitch.tsx
+++ src/modules/ContentSwitch.tsx
@@ -1,33 +1,38 @@
-import React from "react"
+import React, {Suspense, lazy} from 'react';
 import {Route, Switch} from 'react-router-dom'
 import {localRoutes} from "../data/constants";
-import Dashboard from "./dashboard/Dashboard";
-import Contacts from "./contacts/list/Contacts";
-import ContactDetails from "./contacts/details/Details";
-import LoanDetails from "./loans/details/Details";
-import Loans from "./loans/list/LoansList";
+import Loading from "../components/Loading";
 
-import InvoiceDetails from "./loans/invoices/InvoiceDetails";
-import InvoiceList from "./loans/invoices/InvoiceList";
+const Dashboard = lazy(() => import( "./dashboard/Dashboard"));
+const Contacts = lazy(() => import( "./contacts/list/Contacts"));
+const ContactDetails = lazy(() => import( "./contacts/details/Details"));
+const LoanDetails = lazy(() => import( "./loans/details/Details"));
+const Loans = lazy(() => import( "./loans/list/LoansList"));
 
-import Settings from "./settings/Settings";
-import Users from "./admin/users/Users";
-import Home from "./home/Home";
+const InvoiceDetails = lazy(() => import( "./loans/invoices/InvoiceDetails"));
+const InvoiceList = lazy(() => import( "./loans/invoices/InvoiceList"));
+
+const Settings = lazy(() => import( "./settings/Settings"));
+const Users = lazy(() => import( "./admin/users/Users"));
+const Home = lazy(() => import( "./home/Home"));
+
 
 const ContentSwitch = () => {
-    return <Switch>
-        <Route exact={true} path="/" component={Home}/>
-        <Route path={localRoutes.dashboard} component={Dashboard}/>
-        <Route path={localRoutes.contactsDetails} component={ContactDetails}/>
-        <Route path={localRoutes.contacts} component={Contacts}/>
-        <Route path={localRoutes.loansDetails} component={LoanDetails}/>
-        <Route path={localRoutes.loans} component={Loans}/>
-        <Route path={localRoutes.invoicesDetails} component={InvoiceDetails}/>
-        <Route path={localRoutes.invoices} component={InvoiceList}/>
-        <Route path={localRoutes.users} component={Users}/>
-        <Route path={localRoutes.settings} component={Settings}/>
-        <Route component={Home}/>
-    </Switch>
+    return <Suspense fallback={Loading}>
+        <Switch>
+            <Route exact={true} path="/" component={Home}/>
+            <Route path={localRoutes.dashboard} component={Dashboard}/>
+            <Route path={localRoutes.contactsDetails} component={ContactDetails}/>
+            <Route path={localRoutes.contacts} component={Contacts}/>
+            <Route path={localRoutes.loansDetails} component={LoanDetails}/>
+            <Route path={localRoutes.loans} component={Loans}/>
+            <Route path={localRoutes.invoicesDetails} component={InvoiceDetails}/>
+            <Route path={localRoutes.invoices} component={InvoiceList}/>
+            <Route path={localRoutes.users} component={Users}/>
+            <Route path={localRoutes.settings} component={Settings}/>
+            <Route component={Home}/>
+        </Switch>
+    </Suspense>
 }
 
 export default ContentSwitch
